e8066d6d71f7c41d210a6b0f105e6939d32de043
Eva Poulsen <eva@btech.au.dk>
2021-02-12 14:14:06 +0100

    Add files via upload

---
+++ lesson6/dishes.js
@@ -0,0 +1,161 @@
+export const DISHES =
+    [
+        {
+            id: 0,
+            name: 'Uthappiza',
+            image: 'assets/images/pizza.jpg',
+            category: 'mains',
+            label: 'Hot',
+            price: '120 DKr',
+            description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+            comments: [
+                {
+                    id: 0,
+                    rating: 5,
+                    comment: 'This is the best and weirdest pizza on planet Earth!',
+                    author: 'Fake reviewer from Texas',
+                    date: '2021-02-12T12:57:28.556094Z'
+                },
+                {
+                    id: 1,
+                    rating: 2,
+                    comment: 'The combination was just too weird for me. I\'ll stick to traditional Italian pizzas in the future',
+                    author: 'Fake reviewer from Ghent',
+                    date: '2021-02-18T17:52:23.556094Z'
+                },
+                {
+                    id: 2,
+                    rating: 4,
+                    comment: 'Good taste, but could have been spicier. I like it hot!',
+                    author: 'Fake reviewer from Lagos',
+                    date: '2020-12-18T121:20:23.556094Z'
+                },
+                {
+                    id: 3,
+                    rating: 5,
+                    comment: 'Love It!',
+                    author: 'Fake reviewer from Herning',
+                    date: '2018-11-05T21:02:23.556094Z'
+                }
+            ]
+        },
+        {
+            id: 1,
+            name: 'Grandma\'s Tacos',
+            image: 'assets/images/tacos.jpg',
+            category: 'mains',
+            label: '',
+            price: '150 DKr',
+            description: '6 tacos prepared just like my grandma used to. Choose between spicy shredded beef, spicy chicken, the traditional pastor (marinated pork slowly cooked ove direct flames), or a mixed plate.',
+            comments: [
+                {
+                    id: 0,
+                    rating: 5,
+                    comment: 'The best tacos I have tasted outside México',
+                    author: 'Fake reviewer from Guadalajara',
+                    date: '2021-02-12T12:57:28.556094Z'
+                },
+                {
+                    id: 1,
+                    rating: 1,
+                    comment: 'What a soggy mess. I like my tacos in hard shells, the real American style.',
+                    author: 'Fake reviewer from Georgia',
+                    date: '2021-02-18T17:52:23.556094Z'
+                },
+                {
+                    id: 2,
+                    rating: 5,
+                    comment: 'Heaven in a tortilla',
+                    author: 'Fake reviewer from Aarhus',
+                    date: '2020-12-18T121:20:23.556094Z'
+                },
+                {
+                    id: 3,
+                    rating: 5,
+                    comment: 'Only missing a cold Corona',
+                    author: 'Fake reviewer from Lahti',
+                    date: '2018-11-05T21:02:23.556094Z'
+                }
+            ]
+
+        },
+        {
+            id: 2,
+            name: 'Pollo en Mole (Chicken in Spicy Chocolate Sauce)',
+            image: 'assets/images/pollo_mole.jpg',
+            category: 'mains',
+            label: '',
+            price: '215 DKr',
+            description: 'Mexico\'s most iconic mole, this preparation marries mulato, ancho, and pasilla chiles with warm spices to create a velvety smooth sauce ideal for spooning over chicken.',
+            comments: [
+                {
+                    id: 0,
+                    rating: 4,
+                    comment: 'Chocolate and spices really do go well together.',
+                    author: 'Fake reviewer from Wassenberg',
+                    date: '2021-02-12T12:57:28.556094Z'
+                },
+                {
+                    id: 1,
+                    rating: 5,
+                    comment: 'I wasn\'t sure at first, but wow! I could eat this dish every week',
+                    author: 'Fake reviewer from Terrassa',
+                    date: '2021-02-18T17:52:23.556094Z'
+                },
+                {
+                    id: 2,
+                    rating: 1,
+                    comment: 'No, no, no, no... This just was too weird',
+                    author: 'Fake reviewer from Krasnoyarsk',
+                    date: '2020-12-18T121:20:23.556094Z'
+                },
+                {
+                    id: 3,
+                    rating: 4,
+                    comment: 'Good taste, but could have done with some more rice.',
+                    author: 'Fake reviewer from Molepolole',
+                    date: '2018-11-05T21:02:23.556094Z'
+                }
+            ]
+
+        },
+        {
+            id: 3,
+            name: 'Cheesecake',
+            image: 'assets/images/cheesecake.jpg',
+            category: 'desserts',
+            label: 'New',
+            price: '70 DKr',
+            description: 'You have to try it to believe it. Our cheesecake is so creamy and delicious that you will always want an extra slice. Served with a dollop of whipped cream and fresh berries or with cholocate sauce.',
+            comments: [
+                {
+                    id: 0,
+                    rating: 4,
+                    comment: 'Not the Cheesecake Factory, but not bad at all',
+                    author: 'Fake reviewer from Boston',
+                    date: '2021-02-12T12:57:28.556094Z'
+                },
+                {
+                    id: 1,
+                    rating: 2,
+                    comment: 'This cheesecake was too soggy. The crust was anything but crusty',
+                    author: 'Fake reviewer from Viborg',
+                    date: '2021-02-18T17:52:23.556094Z'
+                },
+                {
+                    id: 2,
+                    rating: 4,
+                    comment: 'Good cheesecake.',
+                    author: 'Fake reviewer from Toulouse',
+                    date: '2020-12-18T121:20:23.556094Z'
+                },
+                {
+                    id: 3,
+                    rating: 5,
+                    comment: 'Loved it so much I had 2 slices!',
+                    author: 'Fake reviewer from Porto',
+                    date: '2018-11-05T21:02:23.556094Z'
+                }
+            ]
+        }
+    ];
